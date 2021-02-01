9e6925a483f965b271fad7349188738f1112f70f
adhenrique <adhenriquemusic@gmail.com>
2021-02-01 17:52:43 -0300

    created authcontroller and routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function logIn(Request $request)
+    {
+        $request->validate(
+            [
+                'email'    => 'required|string',
+                'password' => 'required|string',
+            ]
+        );
+
+        if (Auth::guard()->attempt($request->only('email', 'password'))) {
+            $request->session()->regenerate();
+
+            return response()->json([], 204);
+        }
+
+        return response()->json(['error' => 'Invalid credentials'], 401);
+    }
+
+    public function logOut(Request $request)
+    {
+        Auth::guard()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([], 204);
+    }
+}
